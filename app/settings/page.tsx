58cb93289204354f612c3f6e707ecfa8f781c3d3
James Murdza <james@jamesmurdza.com>
2024-04-21 16:37:52 -0700

    Improve image loading UI.

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,16 +6,16 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/ui/spinner';
 
-import { useImageGenerator } from '@/hooks/useImageGenerator'
+import { useImageGenerator } from '@/hooks/useImageGenerator';
 
 export default function SettingsPage() {
   const [prompt, setPrompt] = useState('');
   const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const { loading, handleGenerateImage } = useImageGenerator(); // Using custom hook for loading logic
+  const { loading, handleGenerateImage } = useImageGenerator();
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent default form submission
-    handleGenerateImage(prompt, setImageUrl); // Call the function to generate image
+    e.preventDefault();
+    handleGenerateImage(prompt, setImageUrl);
   };
 
   return (
@@ -23,7 +23,7 @@ export default function SettingsPage() {
       <div className="flex items-center">
         <h1 className="font-semibold text-lg md:text-2xl">Settings</h1>
       </div>
-      <form onSubmit={handleSubmit} className="w-full mb-4 flex items-center">
+      <form onSubmit={handleSubmit} className="w-full flex items-center">
         <Input
           type="text"
           value={prompt}
@@ -31,13 +31,27 @@ export default function SettingsPage() {
           placeholder="Enter an image prompt"
           className="px-4 py-2 border rounded-md flex-grow"
         />
-        <Button type="submit" className="ml-3 py-2 bg-blue-500 text-white rounded-md">
-          Generate Image
+        <Button type="submit" className="ml-3 py-2 bg-blue-500 text-white rounded-md" disabled={loading}>
+          Generate
         </Button>
       </form>
-      <div className="w-full mb-4 text-center">
-        {loading && <Spinner />} {/* Show spinner only when loading */}
-        {imageUrl && !loading && <img src={imageUrl} alt="Generated" />} {/* Show image when loaded and not loading */}
+      <div className="w-full mb-4 text-center relative">
+        {loading && (
+          <div className="border border-input rounded-sm w-64 h-64 relative">
+            {/* Spinner */}
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <Spinner />
+            </div>
+          </div>
+        )}
+        {/* Show image when loaded and not loading */}
+        {imageUrl && !loading && (
+          <img
+            src={imageUrl}
+            alt="Generated"
+            className="border rounded-sm border-input"
+          />
+        )}
       </div>
     </main>
   );
